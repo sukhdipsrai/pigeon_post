fdc5972537eab13ea51d9e8fddffae7edf8a1d8e
Hao <pockyhao518@gmail.com>
2021-02-01 14:49:49 -0500

    auth

--- app.js
+++ app.js
@@ -9,7 +9,27 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
-
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const users = require("./routes/api/users");
+
+app.use("/api/users", users);
+
+
+
+
+
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
