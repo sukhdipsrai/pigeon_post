d45201858a24aaf1f7108807ba5bfe60b2b6db2a
Hao <pockyhao518@gmail.com>
2021-02-03 09:41:10 -0500

    modal

---
+++ frontend/src/components/modal/modal.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { closeModal } from '../../actions/modal_actions';
+import CreateTaskContainer from '../dashboard/task/create_task_container';
+
+function Modal({ modal, closeModal }) {
+    if (!modal) { return null; }
+    let component;
+    switch (modal.modal) {
+        case 'create-task':
+            component = <CreateTaskContainer
+                authorId={modal.authorId}
+            />
+            break;
+        default:
+            return null;
+    }
+
+    return (
+        <div className="modal-background" onClick={closeModal}>
+            <div className="modal-child" onClick={e => e.stopPropagation()}>
+                {component}
+            </div>
+        </div>
+    )
+}
+
+const mSTP = state => {
+    return {
+        modal: state.ui.modal
+    }
+}
+
+const mDTP = dispatch => {
+    return {
+        closeModal: () => dispatch(closeModal())
+    }
+}
+
+export default connect(mSTP, mDTP)(Modal);
