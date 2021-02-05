43d7268266dbe8207293add4c6a64daa9627156d
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-02-04 21:46:47 -0500

    rebased with main

--- frontend/src/components/task_form/gapi_form.js
+++ frontend/src/components/task_form/gapi_form.js
@@ -166,19 +166,6 @@ class GapiForm extends React.Component {
         if (this.state.price !== null && this.state.errors === null) {
             submitButtonValue = "Confirm and Submit";
             priceDisplay =
-<<<<<<< HEAD
-                <div className="price-display-box">
-                    <p>Price Determined {price}</p>
-                    <p>Based on Distance: {distance}</p>
-                    <p>Based on Route Duration: {duration}</p>
-                    <p>Based on Estimated Weight: {weight}</p>
-                    <p>Start Location: {pickup_loc}</p>
-                    <p>Dropoff Location: {dropoff_loc}</p>
-                    <p>Conact number for Delivery: {drop_off_number}</p>
-                    <p>{this.props.user.id}</p>
-                    <button className='task-form-button' onClick={() => this.props.closeModal()} value="Cancel"></button>
-                </div>
-=======
             <div className="price-display-box">
                 <p>Price Determined {price}</p>
                 <p>Based on Distance: {distance}</p>
@@ -189,7 +176,6 @@ class GapiForm extends React.Component {
                 <p>Conact number for Delivery: {drop_off_number}</p>
                 <p>{this.props.user.id}</p>
             </div>
->>>>>>> reworked some form styling and flow
         } else {
             priceDisplay = null;
         }
