d34a423a4108805ac46af2b2d63924ec45fd9dde
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-02-05 11:59:52 -0500

    fixed key naming errors

--- frontend/src/config/keys_prod.js
+++ frontend/src/config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googlekey: process.env.GOOGLE_API_KEY_EXP,
-    googlekeyS: process.env.GOOGLE_API_KEY
+    googlekey: process.env.REACT_APP_GOOGLE_API_KEY_EXP,
+    googlekeyS: process.env.REACT_APP_GOOGLE_API_KEY
   }
