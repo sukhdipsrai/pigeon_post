799fdcefdb0ddc861ca617f0a36abf5d8c280bd7
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-02-16 19:54:33 -0500

    added foot to entire website with links

--- frontend/src/components/splash_page.js
+++ frontend/src/components/splash_page.js
@@ -1,63 +1,46 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import '../stylesheets/splashpage.css'
-import Footer from './foot';
-import splashpaper from '../images/splash-test.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../stylesheets/splashpage.css";
+import Footer from "./foot";
+import splashpaper from "../images/splash-test.png";
 
 // import '../stylesheets/splashpage.css'
-class SplashPage extends React.Component{
-
-
-
-    render(){
-        return (
-            <div>
-            <div className="splashpage-main">
-  
-
-            <div className="left-side">
-
-                <div className="left-side-text">
-
-                <p>PigeonPost is a same-day delivery service that gets your package delivered 
-                    to its destination in a timely manner by our Pigeons, who are 
-                    native New Yorkers who know their way around the 5 boroughs.</p>
-                </div>
-
-                
-                <div className="buttons" >
-                
-                <Link className="links" to="users/signup" >
-                    <button className="splashbuttons">  
-                            Sign up
-                    </button>
-                </Link> 
-                
-                <br/>
-                <br/>
-                
-                <Link className="links" to="users/login" > 
-                        <button className="splashbuttons" > 
-                            Log In
-                        </button>
-                </Link> 
-                </div>  
+class SplashPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="splashpage-main">
+          <div className="left-side">
+            <div className="left-side-text">
+              <p>
+                PigeonPost is a same-day delivery service that gets your package
+                delivered to its destination in a timely manner by our Pigeons,
+                who are native New Yorkers who know their way around the 5
+                boroughs.
+              </p>
             </div>
-            <div className="splashpage-right-side" >
 
-                <img src={splashpaper}/>
+            <div className="buttons">
+              <Link className="links" to="users/signup">
+                <button className="splashbuttons">Sign up</button>
+              </Link>
 
+              <br />
+              <br />
+
+              <Link className="links" to="users/login">
+                <button className="splashbuttons">Log In</button>
+              </Link>
             </div>
-               
-            </div> 
-            <Footer/>
-            </div>
-        )
-    }
-
+          </div>
+          <div className="splashpage-right-side">
+            <img src={splashpaper} />
+          </div>
+        </div>
+        {/* <Footer/> */}
+      </div>
+    );
+  }
 }
 
-
-export default SplashPage
-
-
+export default SplashPage;
