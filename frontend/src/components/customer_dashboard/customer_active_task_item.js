45d6d69829cb9e07025f2c6e43d1b81ff1334ed9
Juan Sanchez <juansanchez721@outlook.com>
2021-02-16 21:14:03 -0500

    Editing active deliveries

--- frontend/src/components/customer_dashboard/customer_active_task_item.js
+++ frontend/src/components/customer_dashboard/customer_active_task_item.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React from 'react'
+import smallpigeon from '../../images/favicon-32x32.png'
+
 
 class ActiveTaskItem extends React.Component {
-  render() {
-    return (
-      <div className="active-task-item">
-        pigeon id: {this.props.task.driver_id}
-        <br />
-        status: {this.props.task.status}
-        <br />
-        weight (lb): {this.props.task.weight}
-        <br />
-        dropoff: {this.props.task.dropoff_loc}
-        <div className="text-div">
-          <p>Price: ${this.props.task.price}</p>
+
+    render(){
+
+        return (
+
+            <div className="active-task-item">
+            <h1>
+            dropoff: 
+                </h1>
+                {this.props.task.dropoff_loc}
+            <br/>
+            <br/>
+            <h1>
+            total weight:   
+            </h1>
+                {this.props.task.weight} (lb)
+            <br/>
+            <br/>
+            <h1>
+                status: 
+                </h1>
+                In transit with pigeon {this.props.task.driver_id}     <img src= {smallpigeon}/>
+            <br/>
+            <div className="text-div">
+                <p>
+                    Contact pigeon
+                    </p>
+            </div>
         </div>
-      </div>
+    //   </div>
     );
   }
 }
