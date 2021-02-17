472382ab66edb5a8a012007f0c0de337c9dfa049
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-02-16 19:24:47 -0500

    fixed dead links

--- frontend/src/components/customer_dashboard/customer_active_task_item.js
+++ frontend/src/components/customer_dashboard/customer_active_task_item.js
@@ -1,30 +1,22 @@
-import React from 'react'
-
+import React from "react";
 
 class ActiveTaskItem extends React.Component {
-
-    render(){
-
-        return (
-
-            <div className="active-task-item">
-            pigeon id: {this.props.task.driver_id}
-            <br/>
-            status: {this.props.task.status}
-            <br/>
-            weight (lb): {this.props.task.weight}
-            <br/>
-            dropoff: {this.props.task.dropoff_loc}
-            <div className="text-div">
-                <p>
-                    Contact pigeon
-                    </p>
-            </div>
+  render() {
+    return (
+      <div className="active-task-item">
+        pigeon id: {this.props.task.driver_id}
+        <br />
+        status: {this.props.task.status}
+        <br />
+        weight (lb): {this.props.task.weight}
+        <br />
+        dropoff: {this.props.task.dropoff_loc}
+        <div className="text-div">
+          <p>Price: ${this.props.task.price}</p>
         </div>
-            )
-    }
-
+      </div>
+    );
+  }
 }
 
-
-export default ActiveTaskItem
+export default ActiveTaskItem;
