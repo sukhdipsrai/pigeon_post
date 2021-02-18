0bf8d7a5ca4dc9a32d40823c082c6e1d375bfb23
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-02-18 17:33:39 -0500

    css overhaul of the task create form is complete

--- frontend/src/components/google_maps/map_container.js
+++ frontend/src/components/google_maps/map_container.js
@@ -2,7 +2,7 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 import React from "react";
 import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 import { googlekey } from "../../config/keys";
-import '../../stylesheets/dashboard.css'
+import "../../stylesheets/dashboard.css";
 const GOOGLE_API_KEY = require("../../config/keys").googlekeyS;
 export class MapContainer extends React.Component {
   constructor(props) {
@@ -13,7 +13,7 @@ export class MapContainer extends React.Component {
     geocodeByAddress(address)
       .then((results) => getLatLng(results[0]))
       .then((latLng) => {
-        console.log("Success", latLng);
+        // console.log("Success", latLng);
         return latLng;
       })
       .catch((error) => console.error("Error", error));
@@ -27,7 +27,7 @@ export class MapContainer extends React.Component {
     };
     return (
       // <div className="map-component-holder">
-        
+
       <Map
         google={this.props.google}
         style={style}
@@ -36,7 +36,7 @@ export class MapContainer extends React.Component {
           lat: 40.754885,
           lng: -73.91223907,
         }}
-        >
+      >
         <Marker position={{ lat: 40.69677842, lng: -73.91223907 }} />
         {/* <Marker
           position={{ lat: 37.759703, lng: -72.428093 }} /> */}
@@ -45,7 +45,7 @@ export class MapContainer extends React.Component {
         <Marker position={{ lat: 40.73503114, lng: -73.8574791 }} />
         <Marker position={{ lat: 40.8234226, lng: -73.86949539 }} />
       </Map>
-// {/* </div> */}
+      // {/* </div> */}
     );
   }
 }
