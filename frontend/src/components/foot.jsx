55c4e0387fc9f1014becf251d42dfec5908a4372
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-02-24 11:57:52 -0500

    add rel attribute for github links

--- frontend/src/components/foot.jsx
+++ frontend/src/components/foot.jsx
@@ -6,19 +6,19 @@ export default class Footer extends React.Component {
         return (
             <div className="div-foot">
                 <ul className='account-link'>
-                    <li><a  target="_blank" href="https://github.com/sukhdipsrai/pigeon_post"><img alt= "" src= {github}/></a></li>
+                    <li><a  target="_blank" rel="noreferrer" href="https://github.com/sukhdipsrai/pigeon_post"><img alt= "" src= {github}/></a></li>
                 </ul>
                 <ul className='account-link'>Sukhdip Rai
-                    <li><a  target="_blank" href="https://github.com/sukhdipsrai">Github</a></li>
-                    <li><a  target="_blank" href="https://www.linkedin.com/in/sukhdip-rai-14766016b/">LinkedIn</a></li>
+                    <li><a  target="_blank" rel="noreferrer" href="https://github.com/sukhdipsrai">Github</a></li>
+                    <li><a  target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/sukhdip-rai-14766016b/">LinkedIn</a></li>
                 </ul>
                 <ul className='account-link'>Juan Sanchez
-                    <li><a  target="_blank" href="https://github.com/juansanchez721">Github</a></li>
-                    <li><a  target="_blank" href="https://www.linkedin.com/in/juan-sanchez-24a68b113/">LinkedIn</a></li>
+                    <li><a  target="_blank" rel="noreferrer" href="https://github.com/juansanchez721">Github</a></li>
+                    <li><a  target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/juan-sanchez-24a68b113/">LinkedIn</a></li>
                 </ul>
                 <ul className='account-link'>Zihao Li
-                    <li><a  target="_blank" href="https://github.com/pockyhao518">Github</a></li>
-                    <li><a  target="_blank" href="https://www.linkedin.com/in/zihao-li-6281b913b/">LinkedIn</a></li>
+                    <li><a  target="_blank" rel="noreferrer" href="https://github.com/pockyhao518">Github</a></li>
+                    <li><a  target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/zihao-li-6281b913b/">LinkedIn</a></li>
                 </ul>
             </div>
         )}}
